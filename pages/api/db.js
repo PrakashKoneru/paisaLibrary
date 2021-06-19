681e2f52facf1abce072b2144435c88faf0defe7
Prakash K <sivaprakashkoneru67@gmail.com>
2021-06-19 01:47:28 -0400

    creating basic library scaffolding

---
+++ pages/api/db.js
@@ -0,0 +1,18 @@
+const Pool = require("pg").Pool;
+
+const pool = () => new Pool({
+	user: process.env.user,
+	password: process.env.poolPass,
+	host: "localhost",
+	port: 5432,
+	database: process.env.db
+});
+
+// pool.end(function(err) {
+// if (err) {
+// 	return console.log('error:' + err.message);
+// }
+// console.log('Close the database connection.');
+// });
+
+module.exports = pool;
